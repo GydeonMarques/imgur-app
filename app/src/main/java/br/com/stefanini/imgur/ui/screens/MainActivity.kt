a8df0bef8cfd5a208f42ab420f5bc690e2601fe3
Gideon Marques da Silva <gmsilva2@stefanini.com>
2024-10-11 07:13:56 -0300

    Base project

---
+++ app/src/main/java/br/com/stefanini/imgur/ui/screens/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.com.stefanini.imgur.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+           //TODO - Implements
+        }
+    }
+}
+
